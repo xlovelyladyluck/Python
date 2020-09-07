8a4a38cc1901c93d78b921be063c634a9ee2b589
xlovelyladyluck <38335658+xlovelyladyluck@users.noreply.github.com>
2020-09-07 11:51:07 -0500

    Add files via upload

---
+++ hello_world_program.py
@@ -0,0 +1,16 @@
+"""
+Program: hello_world_program.py
+Author: Taylor Wilkens
+Last date modified: 09/07/2020
+
+
+
+The purpose of this program is to print "Hello, World!" and call the function
+"""
+
+
+def main():  # This is defining the function
+    print("Hello, World!")  # This is the function body
+
+
+main()  # This is calling the function
