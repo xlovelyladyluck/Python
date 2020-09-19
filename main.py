3bc2d972236009f9b98d511b215d563a6a877539
xlovelyladyluck <38335658+xlovelyladyluck@users.noreply.github.com>
2020-09-18 21:56:52 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,14 @@
-"""
-Program: main.py
-Author: Taylor Wilkens
-Last date modified: 09/13/2020
+subscription_type = str(input("Please choose a membership level from the following: Platinum, Gold, Silver, Bronze or Free Trial."))
 
-
-
-The purpose of this program is to accept 3 test scores, record a students name,
-and output the average of the input test scores
-"""
-
-from format_output.average_scores import average
-
-first_name1 = str(input("Please enter your first name."))
-last_name1 = str(input("Please enter your last name."))
-score1 = float(input("Please enter your first test score"))
-score2 = float(input("Please enter your second test score"))
-score3 = float(input("Please enter your final test score"))
-
-average_score = average(score1, score2, score3)
-
-print(last_name1+ ", "+first_name1+" Average test score: "+str(average_score))
-
-### I felt more confident with this project, and I feel like the debugging is getting easier
-### as I understand how the operations should work as well as what the data types need to be
-### and how the functions need to interact between the file tabs.
+if subscription_type == "platinum":
+    print("You have chosen the Platinum membership. Your monthly fee is $60.")
+elif subscription_type == "gold":
+    print("You have chosen the Gold membership. Your monthly fee is $50.")
+elif subscription_type == "silver":
+    print("You have chosen the Silver membership. Your monthly fee is $40.")
+elif subscription_type == "bronze":
+    print("You have chosen the Bronze membership. Your monthly fee is $30.")
+elif subscription_type == "free trial":
+    print("You have chosen the Free Trial membership. There is no monthly fee.")
+else:
+    print("Your answer is invalid.")
