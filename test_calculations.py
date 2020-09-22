0e00b4cb4ee212e978d279e732c4961eb4e6c4f5
xlovelyladyluck <38335658+xlovelyladyluck@users.noreply.github.com>
2020-09-21 22:18:53 -0500

    Add files via upload

--- test_calculations.py
+++ test_calculations.py
@@ -5,12 +5,24 @@ class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)
 
-class test_price_under_10(unittest.TestCase):
-    def testOne(self):
-        self.assertAlmostEqual(coupon_calculations.calculate_price(15, 5, .1))
-        self.assertAlmostEqual(coupon_calculations.calculate_order(10, 5, 10))
-        self.assertAlmostEqual(coupon_calculations.calculate_order(12, 5, 20))
-        self.assertAlmostEqual(coupon_calculations.calculate_order(10, 5, 20))
+class test_price(unittest.TestCase):
+    def test_price_under_10(self):
+        self.assertAlmostEqual(coupon_calculations.calculate_price(9,5,10))
+        self.assertAlmostEqual(coupon_calculations.calculate_price(10, 5, 10))
+        self.assertAlmostEqual(coupon_calculations.calculate_price(2, 5, 20))
+        self.assertAlmostEqual(coupon_calculations.calculate_price(2, 10,20))
+    def test_price_over_10_under_30(self):
+        self.assertAlmostEqual(coupon_calculations.calculate_price(15, 10, 15))
+        self.assertAlmostEqual(coupon_calculations.calculate_price(20,10,15))
+        self.assertAlmostEqual(coupon_calculations.calculate_price(29,5,10))
+    def test_price_over_30_50(self):
+        self.assertAlmostEqual(coupon_calculations.calculate_price(45, 10,20))
+        self.assertAlmostEqual(coupon_calculations.calculate_price(50, 5, 15))
+        self.assertAlmostEqual(coupon_calculations.calculate_price(36, 5, 10))
+
+
+
+
 
 
 
