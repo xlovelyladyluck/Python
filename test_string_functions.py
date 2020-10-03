1004c29a66e64ff58f70fbc0bbb79d71a06e0590
xlovelyladyluck <38335658+xlovelyladyluck@users.noreply.github.com>
2020-10-03 14:13:33 -0500

    Add files via upload
    mod6 top3 test vr2

--- test_string_functions.py
+++ test_string_functions.py
@@ -4,7 +4,7 @@ from more_functions import string_functions
 
 class MyTestCase(unittest.TestCase):
     def test_multiply_string(self):
-        self.assertEquals(string_functions.multiply_string(), "taylortaylortaylortaylortaylortaylortaylor")
+        self.assertEqual(string_functions.multiply_string(), "taylortaylortaylortaylortaylortaylortaylor")
 
 
 if __name__ == '__main__':
