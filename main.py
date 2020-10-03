ccc4c846b35772f8082ef5591ecdefd2de411bd2
xlovelyladyluck <38335658+xlovelyladyluck@users.noreply.github.com>
2020-10-03 12:17:01 -0500

    Add files via upload
    mod6 topic 1 ver 1

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-subscription_type = str(input("Please choose a membership level from the following: Platinum, Gold, Silver, Bronze or Free Trial."))
+def hourly_employee_input():
+    """"Prompts the user for their name, hours worked, and rate of pay per hour. Prints the user input."""
+    employee_name = input("Enter your name.")
+    try:
+        hours_worked = int(input("Enter hours worked."))
+    except ValueError as err:
+        print("Please input a whole number.")
+        hours_worked = int(input("Enter hours worked."))
+    try:
+        pay_rate = float(input("Enter rate of pay per hour."))
+    except ValueError as err:
+        print("Please input a float only.")
+        pay_rate = float(input("Enter rate of pay per hour."))
 
-if subscription_type == "platinum":
-    print("You have chosen the Platinum membership. Your monthly fee is $60.")
-elif subscription_type == "gold":
-    print("You have chosen the Gold membership. Your monthly fee is $50.")
-elif subscription_type == "silver":
-    print("You have chosen the Silver membership. Your monthly fee is $40.")
-elif subscription_type == "bronze":
-    print("You have chosen the Bronze membership. Your monthly fee is $30.")
-elif subscription_type == "free trial":
-    print("You have chosen the Free Trial membership. There is no monthly fee.")
-else:
-    print("Your answer is invalid.")
+    print(employee_name, " worked ", hours_worked, " for ", pay_rate, " dollars per hour.")
+
+
+if __name__ == '__main__':
+    try:
+        hourly_employee_input()
+    except ValueError as err:
+        print("ValueError encountered.")
