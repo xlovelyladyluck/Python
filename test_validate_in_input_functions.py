325c78bfb8e223d9a6888bf59de6c228106913f6
xlovelyladyluck <38335658+xlovelyladyluck@users.noreply.github.com>
2020-10-04 16:52:00 -0500

    Add files via upload
    test v4 mod6 top4

--- test_validate_in_input_functions.py
+++ test_validate_in_input_functions.py
@@ -6,14 +6,14 @@ class MyTestCase(unittest.TestCase):
    # def test_score_input_test_name(self):
        # self.assertAlmostEqual(validate_input_in_functions.score_input("Science", 0),"Science : 0")
 
-   def test_score_input_test_score_valid(self):
-       self.assertAlmostEqual(validate_input_in_functions.score_input("Science", 100), "Science : 100")
+   #def test_score_input_test_score_valid(self):
+      # self.assertAlmostEqual(validate_input_in_functions.score_input("Science", 100), "Science : 100")
 
-    #def test_score_input_test_score_below_range(self):
-     #   self.assertAlmostEqual(validate_input_in_functions.score_input("Math", -25), "Invalid test score, Try again.")
+    def test_score_input_test_score_below_range(self):
+        self.assertAlmostEqual(validate_input_in_functions.score_input("Math", .011), "invalid literal for int() with base 10: '.011'")
 
-    #def test_score_input_test_score_above_range(self):
-      #  self.assertAlmostEqual(validate_input_in_functions.score_input("English", 125),"Invalid test score, Try again.")
+    def test_score_input_test_score_above_range(self):
+        self.assertAlmostEqual(validate_input_in_functions.score_input("English", 125),"Invalid test score, Try again.")
 
     #def test_test_score_non_numeric(self):
        # self.assertAlmostEqual(validate_input_in_functions.score_input("French", "!!!"), "invalid literal for int() with base 10: '!!!'")
