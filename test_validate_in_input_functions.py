1186d0afcfc1473305f5876c824b7178cbdccf12
xlovelyladyluck <38335658+xlovelyladyluck@users.noreply.github.com>
2020-10-04 16:44:51 -0500

    Add files via upload
    test v3 mod6 top4

--- test_validate_in_input_functions.py
+++ test_validate_in_input_functions.py
@@ -3,11 +3,11 @@ from more_functions import validate_input_in_functions
 
 
 class MyTestCase(unittest.TestCase):
-    def test_score_input_test_name(self):
-        self.assertAlmostEqual(validate_input_in_functions.score_input("Science", 0),"Science : 0")
+   # def test_score_input_test_name(self):
+       # self.assertAlmostEqual(validate_input_in_functions.score_input("Science", 0),"Science : 0")
 
-   #def test_score_input_test_score_valid(self):
-    #    self.assertAlmostEqual(validate_input_in_functions.score_input("Science", 100), "Science : 100")
+   def test_score_input_test_score_valid(self):
+       self.assertAlmostEqual(validate_input_in_functions.score_input("Science", 100), "Science : 100")
 
     #def test_score_input_test_score_below_range(self):
      #   self.assertAlmostEqual(validate_input_in_functions.score_input("Math", -25), "Invalid test score, Try again.")
