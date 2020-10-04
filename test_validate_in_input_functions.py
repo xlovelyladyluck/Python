e6a681462bfb3830df6836b6c3f3e8b84130b025
xlovelyladyluck <38335658+xlovelyladyluck@users.noreply.github.com>
2020-10-04 16:59:42 -0500

    Add files via upload
    v5 mod6 top4

--- test_validate_in_input_functions.py
+++ test_validate_in_input_functions.py
@@ -9,17 +9,17 @@ class MyTestCase(unittest.TestCase):
    #def test_score_input_test_score_valid(self):
       # self.assertAlmostEqual(validate_input_in_functions.score_input("Science", 100), "Science : 100")
 
-    def test_score_input_test_score_below_range(self):
-        self.assertAlmostEqual(validate_input_in_functions.score_input("Math", .011), "invalid literal for int() with base 10: '.011'")
+   # def test_score_input_test_score_below_range(self):
+    #    self.assertAlmostEqual(validate_input_in_functions.score_input("Math", .011), "invalid literal for int() with base 10: '.011'")
 
-    def test_score_input_test_score_above_range(self):
-        self.assertAlmostEqual(validate_input_in_functions.score_input("English", 125),"Invalid test score, Try again.")
+    #def test_score_input_test_score_above_range(self):
+     #   self.assertAlmostEqual(validate_input_in_functions.score_input("English", 125),"Invalid test score, Try again.")
 
-    #def test_test_score_non_numeric(self):
-       # self.assertAlmostEqual(validate_input_in_functions.score_input("French", "!!!"), "invalid literal for int() with base 10: '!!!'")
+    def test_test_score_non_numeric(self):
+        self.assertAlmostEqual(validate_input_in_functions.score_input("French", "!!!"), "invalid literal for int() with base 10: '!!!' ", none)
 
-    #def test_test_score_input_invalid_message(self):
-        #self.assertAlmostEqual(validate_input_in_functions.score_input("French", -14), "Invalid test score, Try again.")
+   def test_test_score_input_invalid_message(self):
+        self.assertAlmostEqual(validate_input_in_functions.score_input("French", -14), "Invalid test score, Try again.")
 
 
 
--- validate_input_in_functions.py
+++ validate_input_in_functions.py
@@ -1,4 +1,4 @@
-def score_input(test_name, test_score, invalid_message):
+def score_input(test_name, test_score=0, invalid_message="Invalid test score, Try Again."):
     """This function is meant to take the test name, score and validate that test score is in range.
     If it's not in range, it is meant to provide an invalid message
 
@@ -6,8 +6,28 @@ def score_input(test_name, test_score, invalid_message):
     @param test_score: This is user input test score. Default value is 0, but must be between 0-100
     @return: If all inputs are valid, the test name will be returned with test score. If invalid,
     an invalid message will be returned.
-    @raise invalid_message: Gives an error message of invalidity"""
+    @raise invalid_message: Gives an error message of invalidity
+    :param invalid_message: "Invalid test score, Try again." """
     # return { test_name: test_score}
-    return pass
+
+    name = input("Please provide the name of your test.")
+    try:
+        score = int(input("Please enter your test score."))
+
+        if 0 <= score <= 100:
+            result = name+" : "+str(score)
+        else:
+            result = invalid_message
+
+    except ValueError as invalid_message:
+        print(invalid_message)
+    else:
+        return result
 
 
+if __name__ == '__main__':
+    print(score_input("Please provide the name of your test.","Enter your test score.", "Invalid test score, Try Again."))
+
+"""I had a really tough time again with this one as far as getting what I wanted, but I felt more confident when I made my tests first as it set the
+idea in place for what I wanted to do first. It did feel like this went a bit better than the ones before as I happened to get the correct out come 
+ through testing repeatedly until I found most of the bugs. I don't think it's perfect, but it is definitely an improvement."""
