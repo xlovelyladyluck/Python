ea97391009c04405af5d113197f22b22ccf4b4e5
xlovelyladyluck <38335658+xlovelyladyluck@users.noreply.github.com>
2020-10-18 15:15:24 -0500

    Add files via upload
    mod8 top1 v2

--- set_membership.py
+++ set_membership.py
@@ -1,2 +1,19 @@
-def in_set():
-    pass
+set_things = {'1', '7', '23', '29', '15', '78'}
+
+
+def in_set(set_list, *x):
+    found = True
+    try:
+        if x in set_list:
+            found
+    except ValueError as err:
+        print("Item does not exist")
+        found = False
+
+    return found
+
+
+result = in_set(set_things, '1')
+
+if result:
+    print("This item is found.")
--- test_set_membership.py
+++ test_set_membership.py
@@ -3,10 +3,10 @@ from more_fun_with_collections import set_membership
 
 class MyTestCase(unittest.TestCase):
     def test_in_set_true(self):
-        self.assertEqual(True, set_membership.in_set("1"))
+        self.assertEqual(True, set_membership.in_set('1'))
 
     def test_in_set_false(self):
-        self.assertEqual(False, set_membership.in_set("fairy"))
+        self.assertEqual(False, set_membership.in_set('99'))
 
 
 if __name__ == '__main__':
